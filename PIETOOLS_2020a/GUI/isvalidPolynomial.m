72998399c43d16eca03b0ed2b5411c337ba6136d
Sachin <sachin-shivakumar@users.noreply.github.com>
2021-04-27 15:46:59 -0700

    Testing valid polynomial inputs

---
+++ PIETOOLS_2020a/GUI/isvalidPolynomial.m
@@ -0,0 +1,12 @@
+function logicalVal = isvalidPolynomial(exp)
+% This function tests whether a mathematical expression exp is a valid
+% polynomial in s and theta
+
+pvar s theta;
+
+exp = convertCharsToStrings(exp); % convert to string
+
+
+
+
+end
