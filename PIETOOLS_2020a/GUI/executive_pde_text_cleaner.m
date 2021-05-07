ab18b6339a36da3af162a201124e0c9f53a92d86
Sachin <sachin-shivakumar@users.noreply.github.com>
2021-05-07 00:00:36 -0700

    GUI with outputs and disturbances

--- PIETOOLS_2020a/GUI/executive_pde_text_cleaner.m
+++ PIETOOLS_2020a/GUI/executive_pde_text_cleaner.m
@@ -5,26 +5,26 @@ PDE_text = strrep(PDE_text, '\theta', 'Theta');
 
 
 for i=1:size(PDE_vars,1)
-    PDE_text = strrep(PDE_text, PDE_vars(i,1), ['X' num2str(i)]);
+    PDE_text = strrep(PDE_text, PDE_vars{i,1}, ['X' num2str(i)]);
 end
 
 for i=1:size(ODE_vars,1)
-    PDE_text = strrep(PDE_text, ODE_vars(i,1), ['x' num2str(i)]);
+    PDE_text = strrep(PDE_text, ODE_vars{i,1}, ['x' num2str(i)]);
 end
 
 for i=1:size(Dis_vars,1)
-    PDE_text = strrep(PDE_text, Dis_vars(i,1), ['w' num2str(i)]);
+    PDE_text = strrep(PDE_text, Dis_vars{i,1}, ['w' num2str(i)]);
 end
 
 for i=1:size(Inp_vars,1)
-    PDE_text = strrep(PDE_text, Inp_vars(i,1), ['u' num2str(i)]);
+    PDE_text = strrep(PDE_text, Inp_vars{i,1}, ['u' num2str(i)]);
 end
 
 for i=1:size(RegOut_vars,1)
-    PDE_text = strrep(PDE_text, RegOut_vars(i,1), ['z' num2str(i)]);
+    PDE_text = strrep(PDE_text, RegOut_vars{i,1}, ['z' num2str(i)]);
 end
 
 for i=1:size(ObsOut_vars,1)
-    PDE_text = strrep(PDE_text, ObsOut_vars(i,1), ['y' num2str(i)]);
+    PDE_text = strrep(PDE_text, ObsOut_vars{i,1}, ['y' num2str(i)]);
 end
 end
